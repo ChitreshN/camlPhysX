afe2ae9f421f685489dbefa62f2cec45e4d462a8
chitreshnarra <chitreshnarra@gmail.com>
2024-04-06 18:47:00 +0530

    feat: implement gravitational potentials

--- lib/object.ml
+++ lib/object.ml
@@ -11,6 +11,8 @@ type obj = {
     mass     : float;
 }
 
+let magnitude v = sqrt (v.x *. v.x +. v.y *. v.y +. v.z *. v.z);;
+
 let vectorSum v1 v2 = { x = v1.x +. v2.x ;
                         y = v1.y +. v2.y ;
                         z = v1.z +. v2.z};;
@@ -19,14 +21,14 @@ let vectorNeg v = { x = 0.0 -. v.x; y = 0.0 -. v.y; z = 0.0 -. v.z};;
 
 let vectorDiff v1 v2 = vectorSum v1 @@ vectorNeg v2;;
 
-let scalarProduct x v = { x = x *. v.x; y = x *. v.y; z = x *. v.z;}
+let scalarProduct x v = { x = x *. v.x; y = x *. v.y; z = x *. v.z;};;
 
-let dotProduct v1 v2 = v1.x *. v2.x +. v1.y *. v2.y +. v1.z *. v2.z
+let dotProduct v1 v2 = v1.x *. v2.x +. v1.y *. v2.y +. v1.z *. v2.z;;
 
 let crossProduct v1 v2 = {  x = v1.y *. v2.z -. v1.z *. v2.y;
                             y = v1.z *. v2.x -. v1.x *. v2.z;
-                            z = v1.x *. v2.y -. v1.y *. v2.x}
+                            z = v1.x *. v2.y -. v1.y *. v2.x};;
 
 let vecCompProduct v1 v2 = {x = v1.x *. v2.x ;
                         y = v1.y *. v2.y ;
-                        z = v1.z *. v2.z}
+                        z = v1.z *. v2.z};;
--- lib/world.ml
+++ lib/world.ml
@@ -6,20 +6,31 @@ let initWorld : world = []
 
 let addObj ob (w : world): world = ob :: w
 
+(*sufficient to remove one instance since it is possible for multiple objects to exist
+ with the same configuration per say*)
 let rec removeObj ob (w : world) : world = 
     match w with
     | [] -> []
     | x::xs -> if  x <> ob then (x :: removeObj ob xs)
-    else removeObj ob xs;;
-
-(*
-let step dt w = 
-    match w with
-    | [] -> []
-    | x::xs -> 
- *)
+    else xs;;
 
 let gravity = {x = 0.; y = -10.; z = 0.}
+
+let g = 1.;; (*place holder for gravitational constant*)
+
+let rec initGravity world = 
+    match world with 
+    | [] -> []
+    | obj::rest -> 
+            let force       = vectorSum obj.force @@ scalarProduct obj.mass gravity in
+            {position = obj.position; velocity = obj.velocity; force = force; mass = obj.mass} :: initGravity rest;;
+
+let rec totalGP world = 
+    match world with
+    | [] -> {x = 0.; y = 0.; z = 0.};
+    | obj::rest -> 
+            let gm = (g *. obj.mass /. (magnitude obj.position)*.(magnitude obj.position)) in
+            vectorSum (scalarProduct gm obj.position) @@ totalGP rest
 
 let rec step dt world = 
     match world with
