ae450a251fe2975f4085eed921f69ccb2109f854
chitreshnarra <chitreshnarra@gmail.com>
2024-04-07 22:16:54 +0530

    feat: add gravitational forces into initGravit fun

--- lib/world.ml
+++ lib/world.ml
@@ -18,19 +18,28 @@ let gravity = {x = 0.; y = -10.; z = 0.}
 
 let g = 1.;; (*place holder for gravitational constant*)
 
-let rec initGravity world = 
-    match world with 
-    | [] -> []
-    | obj::rest -> 
-            let force       = vectorSum obj.force @@ scalarProduct obj.mass gravity in
-            {position = obj.position; velocity = obj.velocity; force = force; mass = obj.mass} :: initGravity rest;;
-
 let rec totalGP world = 
     match world with
     | [] -> {x = 0.; y = 0.; z = 0.};
     | obj::rest -> 
-            let gm = (g *. obj.mass /. (magnitude obj.position)*.(magnitude obj.position)) in
-            vectorSum (scalarProduct gm obj.position) @@ totalGP rest
+            let gm = (g *. obj.mass /. 
+            ((magnitude obj.position)*.(magnitude obj.position)*.(magnitude obj.position))) in
+            vectorSum (scalarProduct gm obj.position) @@ totalGP rest;;
+
+let rec initGravity world = 
+    match world with 
+    | [] -> []
+    | obj::rest -> 
+            let gm          = (g *. obj.mass /. 
+            ((magnitude obj.position)*.(magnitude obj.position)*.(magnitude obj.position))) in
+            let potential   = vectorDiff (totalGP world) (scalarProduct gm obj.position) in
+            let force       = vectorSum obj.force @@ scalarProduct obj.mass gravity in
+            let force       = vectorSum force (scalarProduct obj.mass potential) in
+            {position = obj.position; 
+            velocity = obj.velocity; 
+            force = force; mass = obj.mass} :: initGravity rest;;
+
+
 
 let rec step dt world = 
     match world with
